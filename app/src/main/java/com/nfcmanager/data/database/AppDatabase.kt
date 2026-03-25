d1576e1246b6cb7b116292fa8fce2f9de39d9a74
XACXF <328702091@qq.com>
2026-03-26 06:40:18 +0800

    更新数据库版本到4

--- app/src/main/java/com/nfcmanager/data/database/AppDatabase.kt
+++ app/src/main/java/com/nfcmanager/data/database/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.nfcmanager.data.model.NFCData
 
 @Database(
     entities = [NFCData::class],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
